b31b5b8ab2fb1afaa83ce36d7fa96e301c9edcf0
jaortiz1 <juanantonioortizguerra@gmail.com>
2021-04-06 16:18:45 +0200

    added web spring example at leastgit pullgit pull!

---
+++ pruebas/springExample/src/main/gen/com/redhat/todo/api/PetApiController.java
@@ -0,0 +1,24 @@
+package com.redhat.todo.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-04-06T15:44:10.513+02:00[Europe/Paris]")
+@Controller
+@RequestMapping("${openapi.openAPIPetstore.base-path:/v2}")
+public class PetApiController implements PetApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public PetApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
