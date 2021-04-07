353ceae6eb1707d9283b3d964bfb6ad89140faf6
Juan Antonio Ortiz Guerra <juaortgue@alum.us.es>
2021-04-07 12:52:47 +0200

    aportación a openapi apoyado

--- pruebas/springExample/src/main/gen/com/redhat/todo/api/PetApiController.java
+++ pruebas/springExample/src/main/gen/com/redhat/todo/api/PetApiController.java
@@ -1,15 +1,23 @@
 package com.redhat.todo.api;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
+
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
+
+import com.redhat.todo.model.Pet;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-04-06T15:44:10.513+02:00[Europe/Paris]")
 @Controller
 @RequestMapping("${openapi.openAPIPetstore.base-path:/v2}")
 public class PetApiController implements PetApi {
 
     private final NativeWebRequest request;
+    private @Valid boolean completed;
 
     @org.springframework.beans.factory.annotation.Autowired
     public PetApiController(NativeWebRequest request) {
@@ -20,5 +28,12 @@ public class PetApiController implements PetApi {
     public Optional<NativeWebRequest> getRequest() {
         return Optional.ofNullable(request);
     }
+    
+public ResponseEntity<Pet> getPet(String todoId) {
+    Pet response = new Pet();
+    response.setName("Pet example");
+    response.setId(1l);
+    return ResponseEntity.ok(response);
+}
 
 }
