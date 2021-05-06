b09ebd2b8a55913ed88dde4ce63932e61efd45a7
Nuria <nurgomari@alum.us.es>
2021-05-06 11:21:53 +0200

    MapGameStoreRepository finished

--- secondProject/ProyectoIntegracion-L6-2/src/main/java/aiss/model/repository/MapGameStoreRepository.java
+++ secondProject/ProyectoIntegracion-L6-2/src/main/java/aiss/model/repository/MapGameStoreRepository.java
@@ -1,9 +1,12 @@
 package aiss.model.repository;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import aiss.model.Game;
 import aiss.model.ObjetoStore;
@@ -27,18 +30,90 @@ public class MapGameStoreRepository implements GameStoreRepository{
 	}
 	
 	public void iniData() {
+		Stream<Game> games = MapGameRepository.getInstance().getAllGames().stream();
 		storeMap = new HashMap<String, Store>();
 		//init gamesMap
 		//Create games
 		//Create stores
-		Store game = new Store();
-		game.setName("GAME");
-		game.setLocation("Los Arcos Sevilla");
-		game.setOpenHour(LocalTime.of(8, 0));
-		game.setCloseHour(LocalTime.of(20, 30));
-		game.setPhone("955323867");
-		//anyadir juegos
-		addStore(game);
+		Store store1 = new Store();
+		store1.setName("GAME");
+		store1.setLocation("Los Arcos Sevilla");
+		store1.setOpenHour(LocalTime.of(8, 0));
+		store1.setCloseHour(LocalTime.of(20, 30));
+		store1.setPhone("955323867");
+		List<ObjetoStore> gamesStore1 = new ArrayList<>();
+		ObjetoStore game1Store1 = new ObjetoStore();
+		game1Store1.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("super mario odissey")).findFirst().get());
+		game1Store1.setPrice(49.99);
+		game1Store1.setStock(4);
+		gamesStore1.add(game1Store1);
+		store1.setGames(gamesStore1);
+		addStore(store1);
+		
+		Store store2 = new Store();
+		store2.setName("CeX");
+		store2.setLocation("Paseo de Sant Joan Barcelona");
+		store2.setOpenHour(LocalTime.of(11, 0));
+		store2.setCloseHour(LocalTime.of(21, 0));
+		store2.setPhone("932319555");
+		List<ObjetoStore> gamesStore2 = new ArrayList<>();
+		ObjetoStore game1Store2 = new ObjetoStore();
+		game1Store2.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("fifa 20")).findFirst().get());
+		game1Store2.setPrice(9.50);
+		game1Store2.setStock(2);
+		gamesStore2.add(game1Store2);
+		ObjetoStore game2Store2 = new ObjetoStore();
+		game2Store2.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("animal crossing")).findFirst().get());
+		game2Store2.setPrice(24.95);
+		game2Store2.setStock(3);
+		gamesStore2.add(game2Store2);
+		store2.setGames(gamesStore2);
+		addStore(store2);
+		
+		Store store3 = new Store();
+		store3.setName("GAME");
+		store3.setLocation("Paseo de la Florida Madrid");
+		store3.setOpenHour(LocalTime.of(9, 0));
+		store3.setCloseHour(LocalTime.of(19, 30));
+		store3.setPhone("917168628");
+		List<ObjetoStore> gamesStore3 = new ArrayList<>();
+		ObjetoStore game1Store3 = new ObjetoStore();
+		game1Store3.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("super mario odissey")).findFirst().get());
+		game1Store3.setPrice(45.60);
+		game1Store3.setStock(1);
+		gamesStore3.add(game1Store3);
+		ObjetoStore game2Store3 = new ObjetoStore();
+		game2Store3.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("grand theft auto")).findFirst().get());
+		game2Store3.setPrice(9.50);
+		game2Store3.setStock(5);
+		gamesStore3.add(game2Store3);
+		store3.setGames(gamesStore3);
+		addStore(store3);
+		
+		Store store4 = new Store();
+		store4.setName("RETRO GAME VALENCIA");
+		store4.setLocation("Calle  de l'Arxiduc Carles Valencia");
+		store4.setOpenHour(LocalTime.of(11, 30));
+		store4.setCloseHour(LocalTime.of(20, 0));
+		store4.setPhone("962069197");
+		List<ObjetoStore> gamesStore4 = new ArrayList<>();
+		ObjetoStore game1Store4 = new ObjetoStore();
+		game1Store4.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("animal crossing")).findFirst().get());
+		game1Store4.setPrice(29.65);
+		game1Store4.setStock(2);
+		gamesStore4.add(game1Store4);
+		ObjetoStore game2Store4 = new ObjetoStore();
+		game2Store4.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("grand theft auto")).findFirst().get());
+		game2Store4.setPrice(7.85);
+		game2Store4.setStock(6);
+		gamesStore4.add(game2Store4);
+		ObjetoStore game3Store4 = new ObjetoStore();
+		game3Store4.setGame(games.filter(x -> x.getTitle().toLowerCase().contains("fifa 20")).findFirst().get());
+		game3Store4.setPrice(10.00);
+		game3Store4.setStock(4);
+		gamesStore4.add(game3Store4);
+		store4.setGames(gamesStore4);
+		addStore(store4);
 	}
 	
 	@Override
