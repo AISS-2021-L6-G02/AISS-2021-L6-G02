f14a1521da43e4154acf62a0add7bbf51decae04
albert011 <belatm12@protonmail.com>
2021-05-20 16:09:46 +0200

    descomentado codigo (se me olvido descomentarlo)

--- secondProject/ProyectoIntegracion-L6-2/src/main/java/aiss/api/resources/StoreResource.java
+++ secondProject/ProyectoIntegracion-L6-2/src/main/java/aiss/api/resources/StoreResource.java
@@ -67,7 +67,7 @@ public class StoreResource {
 					.collect(Collectors.toList());
 		}
 
-		/*if (titleGame != null) {
+		if (titleGame != null) {
 			List<Store> aux = new ArrayList<Store>();
 			Boolean predicate = false;
 			int i = 0;
@@ -93,7 +93,7 @@ public class StoreResource {
 
 			}
 			result = aux;
-		}*/
+		}
 
 		if ((order != null)) {
 			switch (order) {
