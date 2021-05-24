7f2d4f8503ac0ab0c03f2f84143663255bf40834
vicgrabru <vicgrabru@alum.us.es>
2021-05-24 16:16:12 +0200

    Arreglado el paquete de repositorio

--- secondProject/ProyectoIntegracion-L6-2/src/main/java/aiss/api/resources/StoreResource.java
+++ secondProject/ProyectoIntegracion-L6-2/src/main/java/aiss/api/resources/StoreResource.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
